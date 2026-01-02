07cfc440af492b53c6aeb93d5c799a01abca3a98
Abhishek Roy <abhishek8400roy@gmail.com>
2026-01-02 15:35:08 +0530

    api key update

--- server/controllers/aicontroller.js
+++ server/controllers/aicontroller.js
@@ -13,8 +13,9 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "models/gemini-1.5-flash",
+  model: "gemini-1.5-flash",
 });
+
 
 /* ================= TEXT FEATURES ================= */
 
